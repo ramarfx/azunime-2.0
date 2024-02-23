7e4d4138d7b948df3857d355b36ea32ea921c6c0
ramarfx <ramagamiing33@gmail.com>
2024-02-23 21:17:13 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-red-500">
+      Hello world!
+    </h1>
+  )
+}
