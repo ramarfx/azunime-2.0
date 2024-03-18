4c784c120ca2d8e2aac6178aa4e6ab518add6725
ramarfx <ramagamiing33@gmail.com>
2024-03-19 06:39:41 +0700

    fix navbar

--- src/components/elements/Input.jsx
+++ src/components/elements/Input.jsx
@@ -2,7 +2,7 @@ const Input = (props) => {
   const { type, name, placeholder, value, onChange, _classname } = props;
   return (
     <input
-      className={`${_classname} bg-secondary-100 text-white focus-visible:outline-none focus-visible:border focus-visible:border-primary px-4 py-3 rounded-lg w-full`}
+      className={`${_classname} bg-secondary-100 text-white focus-visible:outline-none border border-secondary-100 focus-visible:border-primary px-4 py-3 rounded-lg w-full`}
       type={type}
       name={name}
       placeholder={placeholder}
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,17 +1,35 @@
 import { Link } from "react-router-dom";
 import Input from "../elements/Input";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { useRef } from "react";
 
 const Navbar = (props) => {
   const { username, genre } = props;
 
-  return (
-    <section id="navbar" className="w-full bg-secondary-200 fixed z-[999999999]">
-      <div className="container">
-        <div className="flex w-full items-center py-4 gap-10 text-base">
-          <Link to={"/"} className="mr-5 font-montserrat text-2xl font-bold"><span className="text-primary">Azu</span>nime</Link>
+  const hamburgerMenu = useRef(null);
+  const navbar = useRef(null);
 
-          <nav className="flex w-full justify-between items-center">
-            <ul className="flex gap-5">
+  if (hamburgerMenu && navbar) {
+    hamburgerMenu.current.addEventListener("click", () => {
+      navbar.current.classList.toggle("hidden");
+    });
+  }
+
+  return (
+    <section
+      id="navbar"
+      className="fixed z-[999999999] w-full bg-secondary-200"
+    >
+      <div className="container">
+        <div className="relative flex w-full items-center justify-between gap-10 py-4 text-base">
+          <Link to={"/"} className="mr-5 font-montserrat text-2xl font-bold">
+            <span className="text-primary">Azu</span>nime
+          </Link>
+
+          <nav ref={navbar} className="absolute right-0 top-16 flex w-fit flex-col items-start justify-between rounded-b-xl border border-primary bg-secondary-200 p-4 pl-5 md:static md:flex md:w-full md:flex-row md:items-center md:border-none md:p-0">
+            <ul className="flex flex-col gap-5 md:flex-row">
               <li>
                 <Link to={"#"}>Home</Link>
               </li>
@@ -26,12 +44,24 @@ const Navbar = (props) => {
               </li>
             </ul>
 
-            <div className="flex gap-5 items-center">
-              <Input type="text" placeholder="Cari..." _classname="bg-[#303030] py-2"></Input>
-              <Link to={'/login'} className="text-primary font-bold">Login</Link>
-              <Link to={'/register'} className="text-primary font-bold">Register</Link>
+            <div className="flex flex-col-reverse items-start gap-3 md:flex-row md:items-center md:gap-5">
+              <Input
+                type="text"
+                placeholder="Cari..."
+                _classname="bg-[#303030] py-2"
+              ></Input>
+              <div className="my-4 flex items-center gap-5 md:my-0">
+                <FontAwesomeIcon icon={faUser} />
+                <Link to={"/login"} className="py-2 font-bold text-primary">
+                  Login
+                </Link>
+              </div>
             </div>
           </nav>
+
+          <button className="block md:hidden" ref={hamburgerMenu}>
+            <FontAwesomeIcon icon={faBars} className="text-2xl" />
+          </button>
         </div>
       </div>
     </section>
